552053d53b645dc1d919f1ff25e236cabe103923
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-04 23:51:32 +0900

    デプロイ準備４

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'yamasita' && password == '41809'
+      username == '' && password == ''
     end
   end
 end
