c8e5a22590e7d33a1ec609bfa1ee86bf091a382b
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-17 19:16:02 +0900

    アソシエーションの記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :prefecture
+  belongs_to :arrives_day
+  belongs_to :deliverystyle
+
+  validates :name, :explanation, :price, :user, presence: true
+  validates :category_id, :situation_id, :prefecture_id, :arrives_day_id, :deliverystyle_id, numericality: { other_than: 1, message: "can't be blank"}
 end
