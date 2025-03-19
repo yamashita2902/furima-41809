8e7773b2197e884a4a94e9fecdb8768b6ecf9ade
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-19 10:58:48 +0900

    商品出品機能テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,26 @@
 class Item < ApplicationRecord
   validates :name, :explanation, :price, :user, :category_id, :situation_id, :prefecture_id, :arrives_day_id, :delivery_style_id,:image , presence: true
   
-  validates :price, format: { 
-    with: /\A[0-9]+\z/, 
-    message: "Price must be a half-width number" 
-  } 
-  
-  validates :price,  
-    numericality: { 
-    only_integer: true,
-    greater_than_or_equal_to: 300,
-    less_than_or_equal_to: 9_999_999,
-    message: "must be between ¥300 and ¥9,999,999" 
-  }
-  
+
   belongs_to :user
   has_one_attached :image
   validates :category_id, :situation_id, :prefecture_id, :arrives_day_id, :delivery_style_id, numericality: { other_than: 1, message: "can't be blank"}
+  
+  before_validation :check_price_format
 
+  validates :price, presence: true, numericality: { 
+      only_integer: true,
+      greater_than_or_equal_to: 300,
+      less_than_or_equal_to: 9_999_999,
+      message: "must be between ¥300 and ¥9,999,999" 
+    }
+  
+  private
+  
+  def check_price_format
+    unless price.to_s.match?(/\A[0-9]+\z/)
+      errors.add(:price, "must be a half-width number")
+    end
+  end
+  
 end
