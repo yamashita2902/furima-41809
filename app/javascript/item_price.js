9e47b29534fbd7d57590ae5f5ed98ac78bd82125
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-20 00:50:20 +0900

    手数料計算機能の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,14 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const price = () => {
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+  const addProfit = document.getElementById("profit");
+    addProfit.innerHTML = Math.floor(inputValue - inputValue / 10);
+})
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
