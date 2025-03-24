38b0dbe41741856afbc2df3bda836e0ed0e428bc
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-24 14:22:23 +0900

    商品一覧機能修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '商品の状態が未選択（---）では出品できない' do
-        @item.situation_id  = 1
+        @item.situation_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Situation can't be blank")
       end
@@ -70,25 +70,25 @@ RSpec.describe Item, type: :model do
       it '価格が¥300未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be a half-width number and between ¥300 and ¥9,999,999" )
+        expect(@item.errors.full_messages).to include('Price must be a half-width number and between ¥300 and ¥9,999,999')
       end
 
       it '価格が¥9,999,999以上では出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be a half-width number and between ¥300 and ¥9,999,999" )
+        expect(@item.errors.full_messages).to include('Price must be a half-width number and between ¥300 and ¥9,999,999')
       end
 
       it '価格が半角数字でない場合は出品できない' do
-        @item.price = "１０００"
+        @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be a half-width number and between ¥300 and ¥9,999,999" )
+        expect(@item.errors.full_messages).to include('Price must be a half-width number and between ¥300 and ¥9,999,999')
       end
 
       it 'userが紐づいていないと出品できない' do
-        @item.user = nil 
+        @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')  
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
