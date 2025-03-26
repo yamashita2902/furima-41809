5b93590c49b7192bf5dc45bf450da510c022ce6f
yamashita2902 <yptkyyp57575@gmail.com>
2025-03-26 15:24:48 +0900

    destroyアクション修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update,:destroy]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :redirect_unless_owner, only: [:update, :edit,]
+  before_action :set_item, only: [:show, :edit, :update,:destroy]
+  before_action :redirect_unless_owner, only: [:update, :edit,:destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,8 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
